1ae8e63d024bddc54ea910bd58fbcc3a616ae7c1
anilraj <anil050797@gmial.com>
2021-09-03 20:01:32 +0530

    profile done-1

--- src/components/profile/ProfileMainBody.js
+++ src/components/profile/ProfileMainBody.js
@@ -25,39 +25,176 @@ export default class ProfileMainBody extends Component {
 
                 <div className="row">
                     <div className="col">
-                        <div class="accordion accordion-flush" id="accordionFlushExample" style={{color: '#fff !important'}}>
+                        <div class="accordion accordion-flush" id="accordionFlushExample" style={{ color: '#fff !important' }}>
                             <div class="accordion-item">
                                 <h2 class="accordion-header" id="flush-headingOne">
-                                    <button class=" abc collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                    <button class=" acc-btn collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
                                         <h5 className="ct-heading d-flex justify-content-start" >My Account </h5>
-                                        <p className="ct-text d-flex justify-content-start" >Manage your Refund, Payment Mode</p>
+                                        <p className="ct-text d-flex justify-content-start" >Address, Favorites &#38; Offers</p>
                                     </button>
                                 </h2>
                                 <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
                                     <div class="accordion-body">
-                                        <ul>
-                                            <li> <i class="fas fa-home"></i> Mange Address</li>
-                                            <li> <i class="far fa-heart"> </i>  Favorites</li>
-                                            <li> <i class="fas fa-percentage"></i> Offers</li>
+                                        <div className="mt-n-2 u-d-line" ></div>
+                                        <ul className="body-ul">
+                                            <li className="mt-4 " > <a href="#manage-address" className="text-decoration-none ct-heading"> <i class="fas fa-home "></i>  &nbsp; Mange Address <i class="fas fa-greater-than " style={{ color: ' #7e808c' }}></i> </a> </li>
+                                            <li className="mt-4 " > <a href="#favorite" className="text-decoration-none ct-heading"> <i class="fas fa-home "></i>  &nbsp; Favorites <i class="fas fa-greater-than " style={{ color: ' #7e808c' }}></i> </a> </li>
+                                            <li className="mt-4 " > <a href="#offers" className="text-decoration-none ct-heading"> <i class="fas fa-percentage mr-1"></i>  &nbsp;  Offers <i class="fas fa-greater-than " style={{ color: ' #7e808c' }}></i> </a> </li>
                                         </ul>
-
                                     </div>
                                 </div>
                             </div>
                             <div class="accordion-item">
                                 <h2 class="accordion-header" id="flush-headingTwo">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                                        Accordion Item #2
+                                    <button class="acc-btn collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                                        <h5 className="ct-heading d-flex justify-content-start" >Payment &#38; Refunds </h5>
+                                        <p className="ct-text d-flex justify-content-start" >Manage your Refunds, Payment Mode</p>
                                     </button>
                                 </h2>
                                 <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-                                    <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+                                    <div class="accordion-body">
+                                        <div className="mt-n-2 u-d-line" ></div>
+                                        <ul className="body-ul">
+                                            <li className="mt-4 " > <a href="#refunds" className="text-decoration-none ct-heading"> <i class="fas fa-wallet"></i>  &nbsp; Active Refunds <i class="fas fa-greater-than " style={{ color: ' #7e808c' }}></i> </a> </li>
+                                            <li className="mt-4 " > <a href="#payment-mode" className="text-decoration-none ct-heading"> <i class="fas fa-home "></i>  &nbsp; Payment Modes <i class="fas fa-greater-than " style={{ color: ' #7e808c' }}></i> </a> </li>
+                                        </ul>
+                                    </div>
                                 </div>
                             </div>
-                            
+
                         </div>
                     </div>
                 </div>
+                <div className="row mb-2">
+                    <div className="col">
+                        <div className="u-line" ></div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <div className="row">
+                            <div className="col-11">
+                                <h5 className="ct-heading d-flex" >New Swiggy Money <span className="new-tag">NEW</span></h5>
+                                <p className="ct-text">View Account Balance and Transition History</p>
+                            </div>
+                            <div className="col-1 mt-3"> <i class="fas fa-greater-than " style={{ color: ' #7e808c' }}></i></div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row mb-2">
+                    <div className="col">
+                        <div className="u-line" ></div>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col">
+                        <div className="row">
+                            <div className="col-11">
+                                <h5 className="ct-heading d-flex" >New Swiggy Super</h5>
+                                <p className="ct-text">Save big with Free Delivery and More benefits</p>
+                            </div>
+                            <div className="col-1 mt-3"> <i class="fas fa-greater-than " style={{ color: ' #7e808c' }}></i></div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row mb-2">
+                    <div className="col">
+                        <div className="u-line" ></div>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col">
+                        <div className="row">
+                            <div className="col-11">
+                                <h5 className="ct-heading d-flex" >Help</h5>
+                                <p className="ct-text">FAQ &#38; Links</p>
+                            </div>
+                            <div className="col-1 mt-3"> <i class="fas fa-greater-than " style={{ color: ' #7e808c' }}></i></div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="row">
+                    <div className="col">
+                        <div className="row">
+                            <div className="col" style={{ background: '#f4f4f5' }}>
+                                <section>
+                                    <h5 className="ct-heading mt-4 mb-3">PAST ORDERS</h5>
+                                    <div className="pb-4" style={{ background: '#fff' }}>
+                                        <div className="row pt-4" >
+                                            <div className="col-9">
+                                                <h5 className="ct-heading ">Faasos</h5>
+                                                <p className="ct-text mb-0 mt-0">Gandhinager</p>
+                                                <p className="ct-text">₹145 <i class="fas fa-greater-than " style={{ color: ' #7e808c' }}></i> </p>
+                                            </div>
+                                            <div className="col-3">
+                                                <p className="d-flex">Delivered <i class="fas fa-check px-1 py-1 rounded-circle" style={{ background: ' #60b246', color: '#fff', }}></i></p>
+                                            </div>
+                                        </div>
+
+                                        <div className="row mb-2">
+                                            <div className="col">
+                                                <div className="u-d-line" ></div>
+                                            </div>
+                                        </div>
+
+                                        <div className="row">
+                                            <div className="col ">
+                                                <p className="ct-text mb-0">
+                                                <span >Massala Panner Tikka  Wrap. <label>&#40;1&#41;</label> &#44;</span>
+                                                <span>Cheese Melt Panner Wrap. <label>&#40;1&#41;</label> &#44; </span>
+                                                <span>Falefel Nuggets with Mayo Dip. <label>&#40;1&#41;</label></span>
+                                                </p>
+                                                <span className="ct-text mt-2">Nov 18, 2018 10:30 PM</span>
+                                            </div>
+                                        </div>
+
+                                        <div className="row my-2">
+                                            <div className="col d-flex">
+                                                <button className=" sec-btn" style={{color: '#fc8019', borderColor: '#fc8019'}}>REORDER</button>
+                                            </div>
+                                            <div className="col">
+                                                <button className=" sec-btn" style={{color: '#282c3f', borderColor: '#282c3f'}}>RATE FOOD</button>
+                                            </div>
+                                        </div>
+
+                                        <p className="ct-text">You haven't rated this food yet.</p>
+
+                                        <div className="row mt-4 mb-2">
+                                            <div className="col">
+                                                <div className="u-line" ></div>
+                                            </div>
+                                        </div>
+
+                                        
+                                        <a className="sec-btn pl-0 " href="view-more-oder" style={{color: '#fc8019'}}>VIEW MORE ORDERS </a>
+                                        
+                                    </div>
+                                </section>
+
+
+                                <section className="my-4 py-3">
+                                    
+                                   <a href="#logout" className="row sec-btn pl-0 py-2 " style={{color: '#282c3f'}}>
+                                       <div className="col-11">LOGOUT</div>
+                                       <div className="col-1"><i class="fas fa-sign-out-alt"></i></div>
+                                   </a>
+                                </section>
+
+                                <section className="my-4 py-3 d-flex justify-content-center">
+                                    
+                                     <p className="ct-text">&#64;Copyright 2021, New Swiggy Version 1.0.0</p>
+                                </section>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
 
 
             </div>
