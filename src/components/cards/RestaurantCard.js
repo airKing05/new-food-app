911b0454aace36e3dcd22b37debe0d5bfd99e5e2
anilraj <anil050797@gmial.com>
2021-09-06 12:42:46 +0530

    restocard

--- src/components/cards/RestaurantCard.js
+++ src/components/cards/RestaurantCard.js
@@ -1,28 +1,27 @@
 import React from 'react';
 import restocardImg from '../../images/hyderabadi-biryani-pil.png';
-// import {FontAwesome} from 'react-fontawesome'
-// import {faStyles} from 'font-awesome/css/font-awesome.css'
+
 
 
 export default function RestaurantCard() {
     return (
-        <div className="Resto-container">
-            <div className="row-0">
-                <div className="col-deatils">
+        <div className="container Resto-container my-4 rounded" style={{ maxWidth: '500px', color:'#fff', background: '#e46d47' }}>
+            <div className="row row-0">
+                <div className="col-8 col-deatils">
                     <h2>Restaurants</h2>
                     <p>No-Contact Delivery Available</p>
                 </div>
-                <div className="col-img">
-                    <img src={restocardImg} alt="Restaurants-img"/>
+                <div className="col-4 col-img">
+                    <img src={restocardImg} className="img-fluid" alt="Restaurants-img" />
                 </div>
-                </div>
-                <div className="row-1">
-                    <button>
-                        <span>View all  <i className="fas fa-arrow-right"></i></span>
-                    </button>
-                </div>
+            </div>
+            <div className=" row row-1">
+                <button>
+                    <span>View all  <i className="fas fa-arrow-right"></i></span>
+                </button>
+            </div>
         </div>
-        
+
 
     )
 }
