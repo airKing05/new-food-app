0918eb3f75b0d409cfe8001bd7d0e2025a1437f7
anilraj <anil050797@gmial.com>
2021-09-10 23:21:35 +0530

    resto coupon

--- src/components/cards/RestoOfferCoupon/RestoOfferCoupon.js
+++ src/components/cards/RestoOfferCoupon/RestoOfferCoupon.js
@@ -3,9 +3,9 @@ import './RestoOfferCoupon.css'
 
 export default function RestoOfferCoupon() {
     return (
-        <div className=" container border border-1 rounded py-2 mx-2" style={{maxWidth: '500px'}}>
+        <div className=" container border border-1 rounded py-2 mx-2" style={{maxWidth: '350px'}}>
             <div className="text-center">
-            <h3 className="roc-heading-1">&#128512; 50% OFF UPTO ₹100</h3>
+            <h3 className="roc-heading-1 d-block">&#128512; 50% OFF UPTO ₹100</h3>
             <p className="" style={{marginBottom: '0px'}}>USE SPECIALS | ON SELECT ITEMS</p>
             </div>
     </div>
