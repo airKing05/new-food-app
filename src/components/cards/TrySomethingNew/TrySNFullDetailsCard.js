3d03036c98c4b96cd92034f117a5b2b7ef970478
anilraj <anil050797@gmial.com>
2021-09-12 02:22:34 +0530

    classes added

--- src/components/cards/TrySomethingNew/TrySNFullDetailsCard.js
+++ src/components/cards/TrySomethingNew/TrySNFullDetailsCard.js
@@ -11,15 +11,15 @@ export default function TrySNFullDetailsCard() {
                     <div className=" col-5" style={{border: '1px solid blue'}}>
                         <div className="container border rounded" style={{maxWidth: '120px', background:'rgb(223, 32, 32)'}}>
                         <div className="row mt-3">
-                            <div className="col-6" style={{paddingRight: '0px'}}>
-                            <img src={FireFlameImg} alt="fire-flame" className="img-fluid pt-1" style={{minWidth:'37px'}}/>
+                            <div className="col-6 pr-1" >
+                            <img src={FireFlameImg} alt="fire-flame" className="" style={{width:'37px'}}/>
                             </div>
-                            <div className="col-6 pl-1" style={{paddingLeft: '0px'}}>
+                            <div className="col-6 pl-2" >
                                 <div className="tsn-heading-3">Oven <br /> Story</div>
                                 <div className="tsn-text-5">Pizza</div>
                             </div>
                         </div>
-                        <img src={TryNewPizzaImg} alt="food-small-card-img" className="img-fluid" style={{maxWidth:'105px'}}/>
+                        <img src={TryNewPizzaImg} alt="food-small-card-img" className="img-fluid" style={{width:'105px'}}/>
                         {/* <a href="#" style="cursor: pointer;">
                             <!-- <div className="ad-container">
                                             <span>AD</span>
@@ -34,7 +34,7 @@ export default function TrySNFullDetailsCard() {
                 <div className="col-7" style={{border: '1px solid gray'}}>
                     <div className="row">
                         <div className="col-7">
-                        <h4 className="tsn-heading-2">Ovenstory P...</h4>
+                        <div className="tsn-heading-2">Ovenstory P...</div>
                         </div>
                        
                        <div className="col-5 " style={{paddingRight: '0px'}}>
@@ -42,16 +42,16 @@ export default function TrySNFullDetailsCard() {
                         <span className="tsn-text-4 border-top border-right border-bottom py-1 pr-1  d-flex justify-content-end " style={{paddingRight: '0px'}}>BEST SAFETY</span>
                        </div>
                     </div>
-                    <h4 className="tsn-text-2">Pizzas</h4>
-                    <h4  className="tsn-text-2">Talwandi, 2.5 kms</h4>
+                    <div className="tsn-text-2">Pizzas</div>
+                    <div  className="tsn-text-2">Talwandi, 2.5 kms</div>
                     <div className="tsn-text-3" style={{paddingRight: '0px'}}>
                         <span><i className="fas fa-star"> 4.1</i></span>
                         <span> &#8226; 48 mins &#8226; </span>
                         <span> ₹600 for 2</span>
                     </div>
                     <hr />
-                    <p>&#128512; Use TRYNEW </p>
-                    <div className="try-SNFDC-speciality"><i className="far fa-gem" ></i> Pizzas with 4 standout <br/>cheese flavours </div>
+                    <div  className="tsn-text-2">&#128512; Use TRYNEW </div>
+                    <div  className="tsn-text-2"><i className="far fa-gem" ></i> Pizzas with 4 standout <br/>cheese flavours </div>
                     <div className="try-SNFDC-bg-color"></div>
                 </div>
                 </div>
