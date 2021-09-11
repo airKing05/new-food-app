4267c7ab8eca7424a45365d0f85c6e22c668645c
anilraj <anil050797@gmial.com>
2021-09-11 10:59:15 +0530

    tsn

--- src/components/cards/TrySomethingNew/TrySNFullDetailsCard.js
+++ src/components/cards/TrySomethingNew/TrySNFullDetailsCard.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import './TrySNFullDetailsCard.css';
+// import './TrySNFullDetailsCard.css';
 import FireFlameImg from '../../../images/fire-flame.png';
 import TryNewPizzaImg from '../../../images/try-new-pizza.png'
 
 export default function TrySNFullDetailsCard() {
-    return (
+    return ( 
         <>
-            <div className="try-SNFDC-cont">
-                <div className="try-SNFDC-container">
-                    <div className="try-SNFDC-row-img">
-                        <div className="try-SNFDC-img-heading">
+            <div className="try-SNFDC-cont container d-flex" style={{maxWidth: '500px'}}>
+                <div className="try-SNFDC-container row">
+                    <div className="try-SNFDC-row-img col-5" style={{border: '1px solid blue'}}>
+                        {/* <div className="try-SNFDC-img-heading">
                             <img src={FireFlameImg} alt="fire-flame" />
                             <div className="try-SNFDC-img-text">
                                 <h4>Oven <br /> Story</h4>
                                 <span >Pizza</span>
                             </div>
-                        </div>
-                        <img src={TryNewPizzaImg} alt="food-small-card-img" />
+                        </div> */}
+                        <img src={TryNewPizzaImg} alt="food-small-card-img" className="img-fluid"/>
                         {/* <a href="#" style="cursor: pointer;">
                             <!-- <div className="ad-container">
                                             <span>AD</span>
@@ -26,12 +26,12 @@ export default function TrySNFullDetailsCard() {
                             <span> 50 % OFF</span>
                         </div>
                     </div>
-                </div>
-                <div className="try-SNFDC-details">
+                
+                <div className="try-SNFDC-details col-7" style={{border: '1px solid gray'}}>
                     <div className="try-SNFDC-heading">
-                        <h2>Ovenstory P...</h2>
+                        <h4 className="tsn-heading-2">Ovenstory P...</h4>
                         <i className="fal fa-shield-check"></i>
-                        <span className="try-SNFDC-best-safty"><p> BEST SAFETY</p></span>
+                        <span className="try-SNFDC-best-safty tsn-text-4"><p> BEST SAFETY</p></span>
                     </div>
                     <h4>Pizzas</h4>
                     <h4>Talwandi, 2.5 kms</h4>
@@ -44,6 +44,7 @@ export default function TrySNFullDetailsCard() {
                     <p>&#128512; Use TRYNEW </p>
                     <div className="try-SNFDC-speciality"><i className="far fa-gem" ></i> Pizzas with 4 standout <br/>cheese flavours </div>
                     <div className="try-SNFDC-bg-color"></div>
+                </div>
                 </div>
             </div>  
         </>
