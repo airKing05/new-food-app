f46c17fa8143df1b28adbdea29eef7fafe21daab
anilraj <anil050797@gmial.com>
2021-09-13 01:12:05 +0530

    check btn ready

--- src/components/CheckBox/CheckBox.js
+++ src/components/CheckBox/CheckBox.js
@@ -3,9 +3,9 @@ import React from 'react';
 
 export default function CheckBox() {
     return (
-        <div className="d-flex container" style={{fontSize:'20px', fontWeight: '500', letterSpacing: '-1px', lineHeight: '20px'}}>
-            <span style={{fontSize: '40px'}}>
-            <i className="fas fa-check-square pt-1 pr-2" ></i>
+        <div className="d-flex container align-items-center" style={{fontSize:'12px', fontWeight: '500', letterSpacing: '-1px', lineHeight: '15px', padding: '0px'}}>
+            <span style={{fontSize: '30px'}}>
+            <i className="fas fa-check-square pt-1 pr-1" ></i>
             </span>
             <span className="d-flex ">BEST BRANDS</span>
         </div>
--- src/components/CheckBox/CheckBoxCont.js
+++ src/components/CheckBox/CheckBoxCont.js
@@ -3,11 +3,20 @@ import CheckBox from './CheckBox'
 
 export default function CheckBoxCont() {
     return (
-        <div className="container d-flex border border-3 rounded py-2 my-3" style={{padding: '0px'}}>
-            
-              <CheckBox/>
-              <CheckBox/>
-              <CheckBox/>
+        <div className="container d-flex justify-content-evenly border border-3 rounded py-2 my-3" style={{paddingLeft: ' '}}>
+            <div className="row" >
+                <div className="col-4">
+                <CheckBox/>
+                </div>
+                <div className="col-4">
+                <CheckBox/>
+                </div>
+                <div className="col-4">
+                <CheckBox/>
+                </div>
+            </div>
+              
+             
            
            
         </div>
