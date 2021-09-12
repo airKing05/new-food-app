8e947bdeeb6d7eb0e011bdc307533aa8146eac3f
anilraj <anil050797@gmial.com>
2021-09-12 19:09:48 +0530

    nbr without add completed

--- src/components/cards/NearByResto/NearRestoCardWithoutAD.js
+++ src/components/cards/NearByResto/NearRestoCardWithoutAD.js
@@ -4,7 +4,8 @@ import './NearRestoCardWithoutAD.css';
 
 export default function NearRestoCardWithoutAD() {
     return (
-<div className="near-resto-cont">
+<div className="near-resto-cont container">
+  
     <div className="near-resto-small-container">
         <div className="near-resto-woad-row-img">
           <img src={hyderabadiBiryaniImg} alt="food-small-card-img" />
@@ -13,22 +14,25 @@ export default function NearRestoCardWithoutAD() {
                 <span>AD</span>
             </div> -->
           </a> */}
-          <div className="near-resto-offer-box">
+          <div className="d-flex justify-content-center">
+          <a href="off" className="off-btn" style={{top:'131px'}}>
               <span> 40 % OFF</span>
+          </a>
           </div>
         </div>
     </div>
-    <div className="near-resto-woad-details">
-        <h2>The Chicken Factory</h2>
-        <h4>Chiness, Pastas, Beverages, South...</h4>
-        <h4 style={{display: 'flex', justifyContent:'flex-start'}}>Talwandi, 0.4 kms</h4>
-        <div className="reviews">
+    <div className="near-resto-woad-details ">
+        <div className="tsn-heading-2">The Chicken Factory</div>
+        <div className="tsn-text-2 mt-3">Chiness, Pastas, Beverages, South...</div>
+        <div className="tsn-text-2" style={{display: 'flex', justifyContent:'flex-start'}}>Talwandi, 0.4 kms</div>
+        <div className=" tsn-text-3 mt-2">
             <span><i className="fas fa-star"> &#8211;</i></span>
             <span> &#8226; 41mins &#8226;</span>
             <span>₹250 for two</span>
             {/* <!-- <hr>
             <p>&#128512; Buy 2 Get 1 Free </p> --> */}
         </div>
+   
     </div>
 </div>
     )
